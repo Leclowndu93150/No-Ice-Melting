9da2f9e49cc6a7597b6b02d8de3f3905ed8f582b
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-06-07 18:13:47 +0200

    Prevent Snow melting

---
+++ src/main/java/com/leclowndu93150/nom/mixin/SnowBlockMixin.java
@@ -0,0 +1,21 @@
+package com.leclowndu93150.nom.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SnowBlock;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(SnowBlock.class)
+public class SnowBlockMixin {
+    @Inject(method = "randomTick", at = @At("HEAD"), cancellable = true)
+    private void onRandomTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+        // Prevent the snow block from being removed due to light level
+        // Just cancel the method call to do nothing
+        ci.cancel();
+    }
+}
