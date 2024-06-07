3285bda602104983f0095d777d45d852ddeda176
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-06-07 18:05:45 +0200

    works

--- src/main/java/com/leclowndu93150/nom/Main.java
+++ src/main/java/com/leclowndu93150/nom/Main.java
@@ -1,4 +1,4 @@
-package com.leclowndu93150.nom.mixin;
+package com.leclowndu93150.nom;
 
 import net.fabricmc.api.ModInitializer;
 
--- src/main/java/com/leclowndu93150/nom/mixin/IceBlockMixin.java
+++ src/main/java/com/leclowndu93150/nom/mixin/IceBlockMixin.java
@@ -2,39 +2,23 @@ package com.leclowndu93150.nom.mixin;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.IceBlock;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.world.gen.feature.BlueIceFeature;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(IceBlock.class)
-public class IceBlockMixin {
+public abstract class IceBlockMixin {
 
-	// Cancel melting in randomTick
-	@Inject(method = "randomTick", at = @At("HEAD"), cancellable = true)
-	public void preventMeltingRandomTick(BlockState state, ServerWorld world, BlockPos pos, net.minecraft.util.math.random.Random random, CallbackInfo ci) {
-		ci.cancel();
-		System.out.println("Prevented Melting");
-	}
-
-	// Cancel melting in afterBreak
-	@Inject(method = "afterBreak", at = @At("HEAD"), cancellable = true)
-	public void preventMeltingAfterBreak(World world, PlayerEntity player, BlockPos pos, BlockState state, BlockEntity blockEntity, ItemStack tool, CallbackInfo ci) {
-		ci.cancel();
-		System.out.println("Prevented Melting in After Break");
-	}
-
-	// Cancel melting in melt
-	@Inject(method = "melt", at = @At("HEAD"), cancellable = true)
-	protected void preventMeltingMelt(BlockState state, World world, BlockPos pos, CallbackInfo ci) {
-		ci.cancel();
-		System.out.println("Prevented Melting in melt !");
+	@Redirect(
+			method = "randomTick(Lnet/minecraft/block/BlockState;Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/random/Random;)V",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/block/IceBlock;melt(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;)V"
+			)
+	)
+	private void preventMelt(IceBlock instance, BlockState state, World world, BlockPos pos) {
+		// Prevent the melt method from running by not calling it
 	}
 }
